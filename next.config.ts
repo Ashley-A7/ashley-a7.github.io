40ec964f7b12ac0ac804a3c7b6f921bf085f0028
ASHLEY <>
2026-04-07 11:01:59 -0400

    Add portfolio application

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',                // Enables static HTML export
-    basePath: isProd ? '/portfolio' : '',  // Repo name for GitHub Pages
-    assetPrefix: isProd ? '/portfolio/' : '',
-    trailingSlash: true,             // Needed for GitHub Pages folder structure
+    output: 'export',
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio/',
+    turbopack: {
+        root: __dirname,  // explicitly sets your project as root
+    },
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
