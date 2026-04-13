1b3e5e13bccb8d0802d75913e3c2ddb71217b1db
ASHLEY <>
2026-04-12 21:01:14 -0400

    fix app

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ const nextConfig = {
     output: 'export',
     basePath: '/portfolio',
     assetPrefix: '/portfolio/',
-    turbopack: {
-        root: __dirname,  // explicitly sets your project as root
-    },
     images: {
         unoptimized: true,
     },
